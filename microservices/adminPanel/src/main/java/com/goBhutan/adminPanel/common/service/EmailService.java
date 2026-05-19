70d71d6d012c259af9285dc3a90643ab8f3dbdb5
pdrakpa_vendor <pema.drakpa@yagnkhor.com>
2026-05-19 10:41:36 +0600

    changes

---
+++ microservices/adminPanel/src/main/java/com/goBhutan/adminPanel/common/service/EmailService.java
@@ -0,0 +1,27 @@
+package com.goBhutan.adminPanel.common.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendOtp(String toEmail, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("GoBhutan - Password Reset OTP");
+        message.setText(
+                "Your OTP for password reset is: " + otp + "\n\n" +
+                        "This OTP is valid for 5 minutes.\n" +
+                        "If you did not request this, please ignore this email."
+        );
+        mailSender.send(message);
+    }
+}
