00d15b62af7f4d732a7c10d3d8d0e2eda2d2a1d1
pdrakpa_vendor <pema.drakpa@yagnkhor.com>
2026-08-24 14:25:48 +0600

    updates

--- microservices/adminPanel/src/main/java/com/goBhutan/adminPanel/common/config/SecurityConfig.java
+++ microservices/adminPanel/src/main/java/com/goBhutan/adminPanel/common/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -51,6 +53,10 @@ public class SecurityConfig {
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
 		return http.build();
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	// Define global CORS policy
